31df7fff0ab2a71eeab022f87c0bd345e14325c8
Jacob Farkas <jfarkas@lrei.org>
2025-06-17 13:01:05 -0400

    Create download-geojson.sh
    Downloads the ADM2 (Municipalities) GeoJSON file from https://www.geoboundaries.org/

---
+++ download-geojson.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This downloads the ADM2 (Municipalities) GeoJSON file from https://www.geoboundaries.org/
+
+wget https://github.com/wmgeolab/geoBoundaries/raw/main/releaseData/CGAZ/geoBoundariesCGAZ_ADM2.geojson
